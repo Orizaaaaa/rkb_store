9ec9c9546678e9b3d33312236a8c5ce13021cf9c
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-07-26 09:47:37 +0700

    create fetch user

--- src/service/user.ts
+++ src/service/user.ts
@@ -1,21 +1,11 @@
 import { axiosInterceptor } from "./axiosInterceptor"
 
 export const getAllUser = (callback: any) => {
-    axiosInterceptor.get('/user/list')
+    axiosInterceptor.get('/users/list')
         .then((res) => {
             callback(res.data);
         })
         .catch((error) => {
             console.error("Error fetching all unit work:", error);
-        });
-
-}
-
-export const deleteUser = async (id: any, callback: any) => {
-    await axiosInterceptor.delete(`/user/delete/${id}`)
-        .then((result) => {
-            callback(true, result.data)
-        }).catch((err: any) => {
-            console.log(false, err);
         });
 }
